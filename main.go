1b46432f2f20f631d6f4db48750c44cbdb370dd8
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2014-12-15 15:51:49 -0800

    remove unused code

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 	"path/filepath"
 	"runtime"
 	"strconv"
-	"strings"
 	"time"
 )
 
@@ -21,75 +20,13 @@ var (
 	reloadTemplates = true
 )
 
-func StringEmpty(s *string) bool {
-	return s == nil || 0 == len(*s)
-}
-
-func isTopLevelUrl(url string) bool {
-	return 0 == len(url) || "/" == url
-}
-
-func getReferer(r *http.Request) string {
-	return r.Header.Get("Referer")
-}
-
-// Request.RemoteAddress contains port, which we want to remove i.e.:
-// "[::1]:58292" => "[::1]"
-func ipAddrFromRemoteAddr(s string) string {
-	idx := strings.LastIndex(s, ":")
-	if idx == -1 {
-		return s
-	}
-	return s[:idx]
-}
-
-func getIpAddress(r *http.Request) string {
-	hdr := r.Header
-	hdrRealIp := hdr.Get("X-Real-Ip")
-	hdrForwardedFor := hdr.Get("X-Forwarded-For")
-	if hdrRealIp == "" && hdrForwardedFor == "" {
-		return ipAddrFromRemoteAddr(r.RemoteAddr)
-	}
-	if hdrForwardedFor != "" {
-		// X-Forwarded-For is potentially a list of addresses separated with ","
-		parts := strings.Split(hdrForwardedFor, ",")
-		for i, p := range parts {
-			parts[i] = strings.TrimSpace(p)
-		}
-		// TODO: should return first non-local address
-		return parts[0]
-	}
-	return hdrRealIp
-}
-
-func jQueryUrl() string {
-	//return "//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"
-	//return "/js/jquery-1.4.2.js"
-	return "//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min.js"
-}
-
-func prettifyJsUrl() string {
-	//return "//cdnjs.cloudflare.com/ajax/libs/prettify/188.0.0/prettify.js"
-	//return "/js/prettify.js"
-	return "//cdnjs.cloudflare.com/ajax/libs/prettify/r298/prettify.js"
-}
-
-func prettifyCssUrl() string {
-	//return "/js/prettify.css"
-	return "//cdnjs.cloudflare.com/ajax/libs/prettify/r298/prettify.css"
-}
-
-func setContentType(w http.ResponseWriter, contentType string) {
-	w.Header().Set("Content-Type", contentType)
-}
-
 func writeResponse(w http.ResponseWriter, responseBody string) {
 	w.Header().Set("Content-Length", strconv.FormatInt(int64(len(responseBody)), 10))
 	io.WriteString(w, responseBody)
 }
 
 func textResponse(w http.ResponseWriter, text string) {
-	setContentType(w, "text/plain")
+	w.Header().Set("Content-Type", "text/plain")
 	writeResponse(w, text)
 }
 

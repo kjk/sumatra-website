9c30841b2d5f3b920e783bc49e039e9181ea6a5a
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-08-24 23:53:54 -0700

    add scripts/dlsum.sh

---
+++ scripts/dlsum.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+cd www/files
+
+p="http://kjkpub.s3.amazonaws.com/sumatrapdf/rel/"
+
+dlFile() {
+  name=${1}
+  uri="${p}${name}"
+  echo "downloading ${uri} as ${name}"
+  curl -O ${uri}
+}
+
+dlVer() {
+  ver=${1}
+  dlFile "SumatraPDF-${ver}-install.exe"
+  dlFile "SumatraPDF-${ver}.zip"
+  dlFile "SumatraPDF-${ver}-64-install.exe"
+  dlFile "SumatraPDF-${ver}-64.zip"
+}
+
+dlVer "3.1.1"
+dlVer "3.1.2"

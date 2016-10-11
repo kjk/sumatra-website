4eff1eeba797aac8392580400a6c432d0a1b008d
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-10-11 00:00:08 -0700

    fix typo

--- scripts/hyper_update.sh
+++ scripts/hyper_update.sh
@@ -7,5 +7,5 @@ set -o pipefail
 hyper fip detach sumatra-website
 hyper stop sumatra-website
 hyper rm sumatra-website
-hyper run --size=s3 --restart=unless-stopped -d -p 80 --name sumatra-website kjksf/sumara-website:latest
+hyper run --size=s3 --restart=unless-stopped -d -p 80 --name sumatra-website kjksf/sumatra-website:latest
 hyper fip attach 209.177.91.155 sumatra-website

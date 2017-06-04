a9b35b0c9b4367afa4715f814ff7a36c2e7c65a3
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-06-04 14:53:54 -0700

    tag the image properly

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -3,5 +3,5 @@ set -u -e -o pipefail
 
 dep ensure
 GOOS=linux GOARCH=amd64 go build -o sumatra_website_linux -ldflags "-X main.sha1ver=`git rev-parse HEAD`"
-docker build --no-cache --tag sumatra-website:latest .
+docker build --no-cache --tag sumatrawebsite:latest .
 rm sumatra_website_linux

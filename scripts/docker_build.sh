244af478d7beb10cdf657040680834f39767cf9e
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-06-04 14:26:04 -0700

    manage dependencies with dep

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -u -e -o pipefail
 
+dep ensure
 GOOS=linux GOARCH=amd64 go build -o sumatra_website_linux -ldflags "-X main.sha1ver=`git rev-parse HEAD`"
-
 docker build --no-cache --tag sumatra-website:latest .
-
 rm sumatra_website_linux
